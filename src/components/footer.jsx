1bfb2898fc1a7dd0697c8f17a96fb1decafe8bf6
RajanDz <dzaferadzovicrajan@gmail.com>
2025-12-02 10:42:18 +0100

    Some changes are maded

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ export function FooterComponent(){
                 <h1 style={{color: "oklch(.656 .241 354.308)", letterSpacing: "5px", fontSize: "60px", fontWeight: "400"}}>MONAMOUR</h1>
                 <p style={{color: "white", letterSpacing: "5px", fontSize: "30px", fontWeight: "400"}}>POSJETITE NAS</p>
             </div>
-            <p style={{color: "gray" , marginBottom: "20px"}}>© 2025 Monamour. Sva prava zadržana.</p>
+            <p style={{color: "gray" , marginBottom: "20px", width: "max-content"}}>© 2025 Monamour. Sva prava zadržana.</p>
         </div>
     )
 }
