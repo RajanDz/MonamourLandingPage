c5ffeb4eb3cd81e9344e5529ba8d182288546719
RajanDz <dzaferadzovicrajan@gmail.com>
2025-12-02 10:50:04 +0100

    Some changes are maded

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export function FooterComponent(){
     return (
         <div className="footer-section" id="footer">
             <div className="footer-content">
-                <h1 style={{color: "oklch(.656 .241 354.308)", letterSpacing: "5px", fontSize: "60px", fontWeight: "400"}}>MONAMOUR</h1>
+                <h1 className="brand-title">MONAMOUR</h1>
                 <p style={{color: "white", letterSpacing: "5px", fontSize: "30px", fontWeight: "400"}}>POSJETITE NAS</p>
             </div>
             <p style={{color: "gray" , marginBottom: "20px", width: "max-content"}}>© 2025 Monamour. Sva prava zadržana.</p>
